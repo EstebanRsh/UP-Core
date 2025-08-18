22497068953d190393b4f04f066a67f2defbdbd1
Esteban Rusch <ruschestebanalberto081201@gmail.com>
2025-08-18 20:30:56 -0300

    fix(api): crear tablas en startup e importar modelos para registrar metadata

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,18 @@ import sys
 
 sys.tracebacklimit = 1
 from fastapi import FastAPI
-from routes.usuario import Usuario
-from routes.cliente import Cliente
-from routes.plan import Plan
-from routes.contrato import Contrato
-from routes.factura import Factura
+
 from fastapi.middleware.cors import CORSMiddleware
+
+from configs.db import Base, engine
+import models.modelo
+
+# from routes.usuario import Usuario
+# from routes.cliente import Cliente
+# from routes.plan import Plan
+# from routes.contrato import Contrato
+# from routes.factura import Factura
+
 
 api_upcore = FastAPI()
 
@@ -17,11 +23,16 @@ def helloworld():
     return "hello world"
 
 
-api_upcore.include_router(Usuario)
-api_upcore.include_router(Cliente)
-api_upcore.include_router(Plan)
-api_upcore.include_router(Contrato)
-api_upcore.include_router(Factura)
+@api_upcore.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
+
+
+# api_upcore.include_router(Usuario)
+# api_upcore.include_router(Cliente)
+# api_upcore.include_router(Plan)
+# api_upcore.include_router(Contrato)
+# api_upcore.include_router(Factura)
 
 
 api_upcore.add_middleware(
